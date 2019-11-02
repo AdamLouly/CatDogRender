4d7eb84d575f3945e903a25ca528924a8d529411
the 55 <adamlouly3@gmail.com>
2019-11-02 11:43:02 -0600

    adding raw = 1

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/gkr3tf3fsl4ui2v/export.pkl?dl=0'
+export_file_url = 'https://www.dropbox.com/s/gkr3tf3fsl4ui2v/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
 classes = ['cat', 'dog']
